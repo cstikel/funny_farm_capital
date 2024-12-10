86a6a78618a53d5e6ab3ee3992fd9b9f55adf118
Chas Stikeleather <chas@Chass-MacBook-Air.local>
2024-12-10 16:17:19 -0500

    updated stock pitch

--- utils/stock_pitch.py
+++ utils/stock_pitch.py
@@ -9,7 +9,7 @@ def stock_pitch(df, api_key):
     if len(df) > 0:
         stock = df['symbol'].values[0]
         #get pitch for the first stock
-        prompt = f"Write a 2-3 sentence pitch for {stock} using the companies name, in the style of wallstreet bets."
+        prompt = f"Write a 2-3 sentence pitch for the following stock symbol: {stock} , it should use the companies name, in the style of wallstreet bets."
     else:
         #Prommpt for no stocks to make money on today.
         prompt = "Write a 2-3 sentence message about how there is no oppertunity in the market today, in the style of wallstreets bets."
@@ -17,7 +17,7 @@ def stock_pitch(df, api_key):
     message = client.messages.create(
         model="claude-3-5-sonnet-20241022",
         max_tokens=1024,
-        temperature=0.4,
+        temperature=0.2,
         messages=[{"role": "user", "content": prompt} ])
 
     return message.content[0].text
